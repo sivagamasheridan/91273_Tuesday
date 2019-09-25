220193acf28efe469e769af1da284813d72e16e7
ranur789 <ranur@sheridancollege.ca>
2019-09-25 16:03:20 -0400

    commit7

--- src/cardgame/CardTrick.java
+++ src/cardgame/CardTrick.java
@@ -5,14 +5,17 @@
  */
 package cardgame;
 
+import java.util.Scanner;
+
 /**
  *
- * @author srinivsi
+ * @author Ravneet Kaur Ranu
  */
 public class CardTrick {
     public static void main(String[] args) 
     {
       CardGame[] magicHand = new CardGame[7];//array of objects
+      Scanner input = new Scanner(System.in);
       for(int i=0;i<magicHand.length;i++)
       {
       CardGame c1 = new CardGame();//object
@@ -20,8 +23,27 @@ public class CardTrick {
       c1.setSuit(CardGame.SUITS[c1.ranSuit()]);
       magicHand[i] =c1;
       }
-    for(int i=0;i<magicHand.length;i++)
-      {
+        System.out.println("Enter a suit value and a card value");
+        int cardValue = input.nextInt();
+        input.nextLine();
+        String suitValue = input.nextLine();
+        boolean found = false;
+       for(int i=0;i<magicHand.length;i++)
+           {
+          if(cardValue == magicHand[i].getValue() &&
+                  suitValue.equalsIgnoreCase(magicHand[i].getSuit())
+                  ){
+              found = true;
+          break;
+          }
+          if (found)
+          {
+              System.out.println("wow!!! you have your card here!!!!!!!!");
+          }
+          else 
+          {
+              System.out.println("You Loose");
+          }
       System.out.println(magicHand[i].getSuit() +" "+ magicHand[i].getValue());
       
       }
