517c19392c0d50d67180681e1b8da18ba625bbf0
Kdado <dadok@sheridancollege.ca>
2019-09-25 17:47:53 -0400

    ICE 1 Starter Code KEVIN DADO

--- src/cardgame/CardTrick.java
+++ src/cardgame/CardTrick.java
@@ -9,6 +9,7 @@ package cardgame;
  *
  * @author srinivsi
  */
+import java.util.Scanner;
 public class CardTrick {
     public static void main(String[] args) 
     {
@@ -25,8 +26,43 @@ public class CardTrick {
       System.out.println(magicHand[i].getSuit() +" "+ magicHand[i].getValue());
       
       }
-// take input suit and value from user. compare with array.if same card is 
-//in the array print your card is found. 
+    Scanner in = new Scanner(System.in); //input stream
+    //user inputs
+    int suitNumber, user_card_number;
+    String user_suit;
+    
+    //taking user input
+    System.out.println("Enter suit type: 0 for clubs, 1 for hearts, 2 for "
+            + "diamonds, 3 for spades");
+    suitNumber = in.nextInt();
+    System.out.println("Enter card number from 1 to 13");
+    user_card_number = in.nextInt();
+    
+    //invalid card choice
+    if((suitNumber<0 || suitNumber>3) || (user_card_number<1 || user_card_number
+            >13)) {
+        System.out.println("Invalid Inputs");
+    }else {
+        
+        //get the suit type in string
+        user_suit = CardGame.SUITS[suitNumber];
+        
+        int i;
+        //loop for all the cards in magicHand
+        for(i=0;i<magicHand.length;i++)
+        {
+            //Match found, if both attributes are equal
+            if(user_suit.equals(magicHand[i].getSuit())&& user_card_number == 
+                    magicHand[i].getValue()){
+                System.out.println("Your card is found");
+                break;
+            }
+        }
+        //reached at the end of loop but no match found
+        if(i==magicHand.length){
+            System.out.println("Your card is not found");
+        }
+    }
     
     }
 }
