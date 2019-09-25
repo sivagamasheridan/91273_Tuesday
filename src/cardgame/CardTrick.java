cd8634c573d85d1f51bd662143e78bcd8ff35177
Pallavi <sharm640@sheridancollege.ca>
2019-09-25 14:52:39 -0400

    ICE 1 final code

--- src/cardgame/CardTrick.java
+++ src/cardgame/CardTrick.java
@@ -9,6 +9,7 @@ package cardgame;
  *
  * @author Pallavi Sharma
  */
+import java.util.Scanner;
 public class CardTrick {
     public static void main(String[] args) 
     {
@@ -20,9 +21,35 @@ public class CardTrick {
         c1.setSuit(CardGame.SUITS[c1.ranSuit()]);
         magicHand[i]=c1;
         }
+        System.out.println("Enter the card suit and the value");
+        Scanner sc=new Scanner(System.in);
+        String suit=sc.next();
+        int value=sc.nextInt();
+        int win=0;
+        
+        
         for(int i=0;i<magicHand.length;i++)
         {
-            System.out.println(magicHand[i].getSuit()+" "+magicHand[i].getValue());
+            
+            if(suit.equalsIgnoreCase(magicHand[i].getSuit())&&(value==(magicHand[i].getValue())))
+            
+            {
+                win++;
+            }
+           
+                
+        }
+        if(win>0)
+        {
+            System.out.println("The cards matched");
+        }
+        else
+        {
+            System.out.println("The cards didn't match.They were:");
+            for(int i=0;i<magicHand.length;i++)
+            {
+                System.out.println((magicHand[i].getValue())+" of "+(magicHand[i].getSuit()));
+            }
         }
     }
     //take input suit and value from user.compare with array.if same card is 
