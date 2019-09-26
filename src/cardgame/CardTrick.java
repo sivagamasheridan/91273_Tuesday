f6ce996623272f66913f6f52e4331718f1695c06
patenima <patenima@sheridan.desire2learn.com>
2019-09-26 16:18:49 -0400

    nima

--- src/cardgame/CardTrick.java
+++ src/cardgame/CardTrick.java
@@ -5,6 +5,8 @@
  */
 package cardgame;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Nima
@@ -12,12 +14,21 @@ package cardgame;
 public class CardTrick {
     public static void main(String[] args) 
     {
+        System.out.println("Pick a suit and Enter a Card?");
+        Scanner in = new Scanner(System.in);
+         String card1 = in.nextLine();
+         int card = in.nextInt();
+        in.nextLine();
         // TODO code application logic here
         CardGame[] magicHand = new CardGame[7];//array of object
+        
         for(int i= 0; i<magicHand.length; i++){
+            
             CardGame c1 = new CardGame();//an object
+            
             c1.setValue(c1.ranValue());//random number from 1 to 13
             c1.setSuit(CardGame.SUITS[c1.ranSuit()]);
+            
             magicHand[i] = c1;
             System.out.println(magicHand[i].getSuit()+ " "+ 
                     magicHand[i].getValue());
@@ -25,6 +36,16 @@ public class CardTrick {
         
         //take input suit and value from user. compare within the array print
         //your card is found.
+        for(int a=0; a<magicHand.length; a++)
+        {
+            if((magicHand[a].getSuit() == card1)&&(magicHand[a].getValue() == card)) 
+                System.out.println("Card is present");
+            else
+                System.out.println("Card is not present");
+       
+                break;
+        }
+        
 
     }
 }
