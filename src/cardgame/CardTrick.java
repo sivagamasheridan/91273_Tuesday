4bf303e5a8a8ba3a223485eb7cf0e81f0c551ded
patenima <patenima@sheridan.desire2learn.com>
2019-09-26 16:34:02 -0400

    nima

--- src/cardgame/CardTrick.java
+++ src/cardgame/CardTrick.java
@@ -2,6 +2,8 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ *@ Nimaben Patel
+ * Student No.:991542520
  */
 package cardgame;
 
