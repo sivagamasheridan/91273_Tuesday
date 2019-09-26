f1cb84953b431c5cff728397a77577fdb6c5a650
JeromeABrown <jeromebrown@live.ca>
2019-09-26 14:05:44 -0400

    Practicing my GITHUB

---
+++ src/cardgame/CardGame.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cardgame;
+
+/**
+ *
+ * @author srinivsi
+ date 24,sep 2019
+ ICE 1 activity */
+public class CardGame 
+{
+private String suit;// spades,diamonds,clubs,hearts
+private int value; // 1 to 13
+public static final String[] SUITS ={"clubs","hearts","diamonds","spades"};
+    /**
+     * @return the suit
+     */
+    public String getSuit() {
+        return suit;
+    }
+
+    /**
+     * @param suit the suit to set
+     */
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    /**
+     * @return the value
+     */
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(int value) {
+        this.value = value;
+    }
+ //you have to write two methods for random generation of suit and value
+public int ranSuit()
+{
+    int value= (int)(Math.random()*4)+0;
+    return value;
+}
+public int ranValue()
+{
+ int value= (int)(Math.random()*13)+1;   
+ return value;
+}
+}
---
+++ src/cardgame/CardTrick.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cardgame;
+
+/**
+ *
+ * @author srinivsi
+ */
+public class CardTrick {
+    public static void main(String[] args) 
+    {
+      CardGame[] magicHand = new CardGame[7];//array of objects
+      for(int i=0;i<magicHand.length;i++)
+      {
+      CardGame c1 = new CardGame();//object
+      c1.setValue(c1.ranValue());//random number 1 to 13
+      c1.setSuit(CardGame.SUITS[c1.ranSuit()]);
+      magicHand[i] =c1;
+      }
+    for(int i=0;i<magicHand.length;i++)
+      {
+      System.out.println(magicHand[i].getSuit() +" "+ magicHand[i].getValue());
+      
+      }
+// take input suit and value from user. compare with array.if same card is 
+//in the array print your card is found.
+    
+    
+    }
+}
